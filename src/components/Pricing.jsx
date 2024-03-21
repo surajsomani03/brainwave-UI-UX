b6103bef18c19676d5f5b8ad3e7d07ac65e7d911
Suraj Somani <surajsomani2002@gmail.com>
2024-03-21 23:15:41 +0530

    Roadmap and footer section design completed

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,11 +1,12 @@
+import Section from "./Section";
 import { smallSphere, stars } from "../assets";
 import Heading from "./Heading";
 import PricingList from "./PricingList";
-import Section from "./Section";
 import { LeftLine, RightLine } from "./design/Pricing";
+
 const Pricing = () => {
   return (
-    <Section className="overflow-hidden">
+    <Section className="overflow-hidden" id="pricing">
       <div className="container relative z-2">
         <div className="hidden relative justify-center mb-[6.5rem] lg:flex">
           <img
@@ -13,7 +14,7 @@ const Pricing = () => {
             className="relative z-1"
             width={255}
             height={255}
-            alt="sphere"
+            alt="Sphere"
           />
           <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
             <img
@@ -21,10 +22,11 @@ const Pricing = () => {
               className="w-full"
               width={950}
               height={400}
-              alt="stars"
+              alt="Stars"
             />
           </div>
         </div>
+
         <Heading
           tag="Get started with Brainwave"
           title="Pay once, use forever"
@@ -35,12 +37,13 @@ const Pricing = () => {
           <LeftLine />
           <RightLine />
         </div>
+
         <div className="flex justify-center mt-10">
           <a
             className="text-xs font-code font-bold tracking-wider uppercase border-b"
             href="/pricing"
           >
-            See the full details.
+            See the full details
           </a>
         </div>
       </div>
